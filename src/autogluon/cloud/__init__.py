c52c0a31f68e37329553f4fa0a577c2af1c15d11
Weisu Yin <weisy@amazon.com>
2022-12-16 10:25:55 -0800

    Move cloud src code (#1)
    * move cloud src code
    
    * move one level up

---
+++ src/autogluon/cloud/__init__.py
@@ -0,0 +1,5 @@
+from autogluon.common.utils.log_utils import _add_stream_handler
+
+from .predictor import ImageCloudPredictor, MultiModalCloudPredictor, TabularCloudPredictor, TextCloudPredictor
+
+_add_stream_handler()
