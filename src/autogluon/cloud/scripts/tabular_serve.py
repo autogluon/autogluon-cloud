e3a66f035fbd8e7950e56385ee2b3adc081a9676
Weisu Yin <weisy@amazon.com>
2022-12-22 20:25:54 +0000

    format

--- src/autogluon/cloud/scripts/tabular_serve.py
+++ src/autogluon/cloud/scripts/tabular_serve.py
@@ -18,6 +18,7 @@ def _cleanup_images():
         if file.endswith(".png"):
             os.remove(file)
 
+
 def _save_image_and_update_dataframe_column(bytes):
     im_bytes = base64.b85decode(bytes)
     # nosec B303 - not a cryptographic use
@@ -92,7 +93,7 @@ def transform_fn(model, request_body, input_content_type, output_content_type="a
         output = prediction.to_csv(index=None)
     else:
         raise ValueError(f"{output_content_type} content type not supported")
-    
+
     _cleanup_images()
 
     return output, output_content_type
