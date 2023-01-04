ef7ea7e3200f1d3b14f808567ca3885152dc41bf
Weisu Yin <weisy@amazon.com>
2023-01-04 23:41:37 +0000

    test

--- .github/workflow_scripts/build_doc.sh
+++ .github/workflow_scripts/build_doc.sh
@@ -12,9 +12,9 @@ source $(dirname "$0")/env_setup.sh
 if [[ (-n $PR_NUMBER) || ($GIT_REPO != "autogluon/autogluon-cloud") ]]
 then
     # Put in cloud bucket for staging purpose
-    bucket='autogluon-cloud-doc-staging'
+    BUCKET='autogluon-cloud-doc-staging'
     if [[ -n $PR_NUMBER ]]; then path=$PR_NUMBER; else path=$BRANCH; fi
-    site=$bucket.s3-website-us-west-2.amazonaws.com/$path/$COMMIT_SHA  # site is the actual bucket location that will serve the doc
+    site=$BUCKET.s3-website-us-west-2.amazonaws.com/$path/$COMMIT_SHA  # site is the actual bucket location that will serve the doc
 else
     if [[ $BRANCH == "master" ]]
     then
@@ -25,8 +25,8 @@ else
     else
         exit 0  # For other branch pushed to autogluon-cloud. We do not build docs.
     fi
-    bucket='autogluon.mxnet.io'
-    site=$bucket/$path  # site is the actual bucket location that will serve the doc
+    BUCKET='autogluon.mxnet.io'
+    site=$BUCKET/$path  # site is the actual bucket location that will serve the doc
 fi
 
 other_doc_version_text='Stable Version Documentation'
@@ -54,12 +54,12 @@ if [[ (-n $PR_NUMBER) || ($GIT_REPO != "autogluon/autogluon-cloud") ]]
 then
     # If PR, move the whole doc folder (to keep css styles) to staging bucket for visibility
     DOC_PATH=_build/html/
-    S3_PATH=s3://$bucket/$path/$COMMIT_SHA
+    S3_PATH=s3://$BUCKET/$path/$COMMIT_SHA
     aws s3 cp $DOC_PATH $S3_PATH --recursive
 else
     # If master/stable, move the individual tutorial html to dev/stable bucket of main AG
     cacheControl='--cache-control max-age=7200'
     DOC_PATH=_build/html/tutorials/autogluon-cloud.html
-    S3_PATH=s3://$bucket/$path/tutorials/cloud_fit_deploy/
+    S3_PATH=s3://$BUCKET/$path/tutorials/cloud_fit_deploy/
     aws s3 cp $DOC_PATH $S3_PATH --acl public-read ${cacheControl}
 fi
