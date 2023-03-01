fb2d26c6e722f4615a6781d9c13b21f6a7113c20
Weisu Yin <weisy@amazon.com>
2023-03-01 21:59:06 +0000

    fix

--- src/autogluon/cloud/cluster/__init__.py
+++ src/autogluon/cloud/cluster/__init__.py
@@ -1,1 +1,3 @@
-from .ray_aws_cluster_config_generator import ClusterConfigGenerator, RayAWSClusterConfigGenerator
+from .cluster_config_generator import ClusterConfigGenerator
+from .ray_cluster_config_generator import RayClusterConfigGenerator
+from .ray_aws_cluster_config_generator import RayAWSClusterConfigGenerator
