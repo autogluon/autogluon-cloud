82b4594eaf613bec0a9e93d087711ce888074183
Weisu Yin <weisy@amazon.com>
2023-03-03 00:30:14 +0000

    check

--- src/autogluon/cloud/cluster/ray_aws_cluster_manager.py
+++ src/autogluon/cloud/cluster/ray_aws_cluster_manager.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import json
 import logging
 import os
@@ -64,3 +65,35 @@ class RayAWSClusterManager(RayClusterManager):
         )
 
         return {"trust_relationship": trust_relationship_file_path, "iam_policy": iam_policy_file_path}
+=======
+from typing import Dict
+
+from .ray_cluster_manager import RayClusterManager
+
+
+class RayAWSClusterManager(RayClusterManager):
+    @staticmethod
+    def generate_default_permission() -> Dict[str, str]:
+        """
+        Generate trust relationship and iam policy required to manage cluster
+        """
+        raise NotImplementedError
+
+    def setup_connection(self, port) -> None:
+        """
+        Setup connection between local and remote ray cluster to enable job submission
+        """
+        raise NotImplementedError
+
+    def setup_dashboard(self, port) -> None:
+        """
+        Setup ray dashboard to monitor cluster status
+        """
+        raise NotImplementedError
+
+    def exec(self, command) -> None:
+        """
+        Execute the command on the head node of the cluster
+        """
+        raise NotImplementedError
+>>>>>>> check
