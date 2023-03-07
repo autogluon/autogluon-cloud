48ad7b27a7e8467b9e2ebb712a5073e7e64bdd42
Weisu Yin <weisy@amazon.com>
2023-03-07 16:57:50 +0000

    enable tests

--- .github/workflow_scripts/test_cloud.sh
+++ .github/workflow_scripts/test_cloud.sh
@@ -7,14 +7,14 @@ set -ex
 
 source $(dirname "$0")/env_setup.sh
 
-# if [ $MODULE = "tabular" ]
-# then
-#     install_tabular $AG_VERSION
-# elif [ $MODULE = "multimodal" ]
-# then
-#     install_multimodal $AG_VERSION
-# fi
+if [ $MODULE = "tabular" ]
+then
+    install_tabular $AG_VERSION
+elif [ $MODULE = "multimodal" ]
+then
+    install_multimodal $AG_VERSION
+fi
 
-# install_cloud_test
+install_cloud_test
 
-# python3 -m pytest -n 2 --junitxml=results.xml tests/unittests/$MODULE/ --framework_version $AG_VERSION
+python3 -m pytest -n 2 --junitxml=results.xml tests/unittests/$MODULE/ --framework_version $AG_VERSION
