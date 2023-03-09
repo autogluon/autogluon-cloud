ad815ea73f54f29e0d5b8d2a5ec41bd4bb0c39aa
Weisu Yin <weisy@amazon.com>
2023-03-09 18:23:08 +0000

    test

--- tests/unittests/cluster/test_ray_job.py
+++ tests/unittests/cluster/test_ray_job.py
@@ -1,26 +1,29 @@
 import subprocess
 
+import pytest
+import tempfile
+import os
+
 from autogluon.cloud.job.ray_job import RayJob
-
-# import pytest
-
 
 
 # @pytest.mark.local_only
 def test_ray_job():
     # Create a local cluster to test ray job
-    try:
-        result = subprocess.run(["ray", "start", "--head"], capture_output=True, check=True)
-        print(result.stdout)
-        job = RayJob()
-        job.run(entry_point="echo hi", runtime_env=None, wait=True)
-        info = job.info()
-        assert info["status"] == "SUCCEEDED"
-        job2 = RayJob()
-        job2_name = "dummy"
-        job2.run(entry_point="echo hi", runtime_env=None, wait=False, job_name=job2_name)
-        job3 = RayJob.attach(job_name=job2_name)
-        info = job3.info()
-        assert info["status"] == "SUCCEEDED"
-    finally:
-        subprocess.run(["ray", "stop"])
+    with tempfile.TemporaryDirectory() as temp_dir:
+        os.chdir(temp_dir)
+        try:
+            result = subprocess.run(["ray", "start", "--head"], capture_output=True, check=True)
+            print(result.stdout)
+            job = RayJob()
+            job.run(entry_point="echo hi", runtime_env=None, wait=True)
+            info = job.info()
+            assert info["status"] == "SUCCEEDED"
+            job2 = RayJob()
+            job2_name = "dummy"
+            job2.run(entry_point="echo hi", runtime_env=None, wait=False, job_name=job2_name)
+            job3 = RayJob.attach(job_name=job2_name)
+            info = job3.info()
+            assert info["status"] == "SUCCEEDED"
+        finally:
+            subprocess.run(["ray", "stop"])
