37bc9100be7cf24ff9ba84ba45e7267716eb130e
Weisu Yin <weisy@amazon.com>
2023-03-09 19:42:02 +0000

    test

--- tests/unittests/cluster/test_ray_job.py
+++ tests/unittests/cluster/test_ray_job.py
@@ -1,29 +1,31 @@
-# import subprocess
+import subprocess
 
-# import pytest
-# import tempfile
-# import os
+import pytest
+import tempfile
+import os
 
-# from autogluon.cloud.job.ray_job import RayJob
+from autogluon.cloud.job.ray_job import RayJob
 
 
-# # @pytest.mark.local_only
-# def test_ray_job():
-#     # Create a local cluster to test ray job
-#     with tempfile.TemporaryDirectory() as temp_dir:
-#         os.chdir(temp_dir)
-#         try:
-#             result = subprocess.run(["ray", "start", "--head"], capture_output=True, check=True)
-#             print(result.stdout)
-#             job = RayJob()
-#             job.run(entry_point="echo hi", runtime_env=None, wait=True)
-#             info = job.info()
-#             assert info["status"] == "SUCCEEDED"
-#             job2 = RayJob()
-#             job2_name = "dummy"
-#             job2.run(entry_point="echo hi", runtime_env=None, wait=False, job_name=job2_name)
-#             job3 = RayJob.attach(job_name=job2_name)
-#             info = job3.info()
-#             assert info["status"] == "SUCCEEDED"
-#         finally:
-#             subprocess.run(["ray", "stop"])
+# @pytest.mark.local_only
+def test_ray_job():
+    # Create a local cluster to test ray job
+    with tempfile.TemporaryDirectory() as temp_dir:
+        os.chdir(temp_dir)
+        try:
+            os.environ.pop("RAY_ADDRESS", None)
+            dashboard_port = "8266"  # not using the default 8265 to avoid conflicts
+            address = f"http://127.0.0.1:{dashboard_port}"
+            result = subprocess.run(["ray", "start", "--head", "--dashboard-port", dashboard_port], capture_output=True, check=True)
+            job = RayJob(address=address)
+            job.run(entry_point="echo hi", runtime_env=None, wait=True)
+            info = job.info()
+            assert info["status"] == "SUCCEEDED"
+            job2 = RayJob(address=address)
+            job2_name = "dummy"
+            job2.run(entry_point="echo hi", runtime_env=None, wait=False, job_name=job2_name)
+            job3 = RayJob.attach(job_name=job2_name, address=address)
+            info = job3.info()
+            assert info["status"] == "SUCCEEDED"
+        finally:
+            subprocess.run(["ray", "stop"])
