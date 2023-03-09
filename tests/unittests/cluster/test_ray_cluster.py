158f82bd3866570cf0f5294c0cdbb4a4121d162a
Weisu Yin <weisy@amazon.com>
2023-03-09 01:16:48 +0000

    lint

--- tests/unittests/cluster/test_ray_cluster.py
+++ tests/unittests/cluster/test_ray_cluster.py
@@ -3,7 +3,6 @@ import tempfile
 import time
 
 from autogluon.cloud.cluster import RayAWSClusterConfigGenerator, RayAWSClusterManager
-from autogluon.cloud.job.ray_job import RayJob
 
 
 def test_ray_aws_cluster(test_helper):
