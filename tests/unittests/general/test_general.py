2bbaa83c11f2a145d415600b6852220f397a0545
Weisu Yin <weisy@amazon.com>
2023-03-22 13:26:40 -0700

    Fix general unit test (#56)

--- tests/unittests/general/test_general.py
+++ tests/unittests/general/test_general.py
@@ -1,13 +1,13 @@
 import json
 import tempfile
 
-from autogluon.cloud import TabularCloudPredictor
+from autogluon.cloud.backend import SagemakerBackend
 
 
 def test_generate_trust_relationship_and_iam_policy():
     with tempfile.TemporaryDirectory() as root:
-        cp = TabularCloudPredictor("dummy")
-        paths = cp.generate_default_permission(account_id="foo", cloud_output_bucket="foo", output_path=root)
+        backend = SagemakerBackend(local_output_path="dummy", cloud_output_path="dummy", predictor_type="dummy")
+        paths = backend.generate_default_permission(account_id="foo", cloud_output_bucket="foo", output_path=root)
         trust_relationship_path, iam_policy_path = paths["trust_relationship"], paths["iam_policy"]
         for path in [trust_relationship_path, iam_policy_path]:
             with open(path, "r") as file:
