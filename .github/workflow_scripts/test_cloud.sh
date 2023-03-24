d802fb449dc4b5aef1ad3130f34cb310fcc5951b
Weisu Yin <weisy@amazon.com>
2023-03-24 20:31:43 +0000

    fix

--- .github/workflow_scripts/test_cloud.sh
+++ .github/workflow_scripts/test_cloud.sh
@@ -7,6 +7,8 @@ set -ex
 
 source $(dirname "$0")/env_setup.sh
 
+install_cloud_test
+
 if [ $MODULE = "tabular" ]
 then
     install_tabular $AG_VERSION
@@ -14,7 +16,5 @@ elif [ $MODULE = "multimodal" ]
 then
     install_multimodal $AG_VERSION
 fi
-
-install_cloud_test
 
 python3 -m pytest -n 2 --junitxml=results.xml tests/unittests/$MODULE/ --framework_version $AG_VERSION
