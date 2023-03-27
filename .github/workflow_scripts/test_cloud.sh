cf26244525750b49054f447718bac2587b5e097e
Weisu Yin <weisy@amazon.com>
2023-03-27 21:58:17 +0000

    test

--- .github/workflow_scripts/test_cloud.sh
+++ .github/workflow_scripts/test_cloud.sh
@@ -1,20 +1,20 @@
-#!/bin/bash
+# #!/bin/bash
 
-MODULE=$1
-AG_VERSION="${2:-source}"
+# MODULE=$1
+# AG_VERSION="${2:-source}"
 
-set -ex
+# set -ex
 
-source $(dirname "$0")/env_setup.sh
+# source $(dirname "$0")/env_setup.sh
 
-install_cloud_test
+# install_cloud_test
 
-if [ $MODULE = "tabular" ]
-then
-    install_tabular $AG_VERSION
-elif [ $MODULE = "multimodal" ]
-then
-    install_multimodal $AG_VERSION
-fi
+# if [ $MODULE = "tabular" ]
+# then
+#     install_tabular $AG_VERSION
+# elif [ $MODULE = "multimodal" ]
+# then
+#     install_multimodal $AG_VERSION
+# fi
 
-python3 -m pytest -n 2 --junitxml=results.xml tests/unittests/$MODULE/ --framework_version $AG_VERSION
+# python3 -m pytest -n 2 --junitxml=results.xml tests/unittests/$MODULE/ --framework_version $AG_VERSION
--- .github/workflow_scripts/test_cluster.sh
+++ .github/workflow_scripts/test_cluster.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+# #!/bin/bash
 
-set -ex
+# set -ex
 
-source $(dirname "$0")/env_setup.sh
+# source $(dirname "$0")/env_setup.sh
 
-install_cloud_test
+# install_cloud_test
 
-python3 -m pytest --forked --junitxml=results.xml tests/unittests/cluster/
+# python3 -m pytest --forked --junitxml=results.xml tests/unittests/cluster/
