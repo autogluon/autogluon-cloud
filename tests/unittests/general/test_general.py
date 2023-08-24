96631eeb953b0f5c149c8eecb72554c881af547d
Weisu Yin <weisy@amazon.com>
2023-08-24 17:46:44 +0000

    add tests

--- tests/unittests/general/test_general.py
+++ tests/unittests/general/test_general.py
@@ -1,13 +1,18 @@
 import json
 import tempfile
 
-from autogluon.cloud.backend import SagemakerBackend
+import pytest
+
+from autogluon.cloud import TabularCloudPredictor
+from autogluon.cloud.backend.constant import RAY_AWS, SAGEMAKER
 
 
-def test_generate_trust_relationship_and_iam_policy():
+@pytest.mark.parametrize("backend", [RAY_AWS, SAGEMAKER])
+def test_generate_default_permission(backend):
     with tempfile.TemporaryDirectory() as root:
-        backend = SagemakerBackend(local_output_path="dummy", cloud_output_path="dummy", predictor_type="dummy")
-        paths = backend.generate_default_permission(account_id="foo", cloud_output_bucket="foo", output_path=root)
+        paths = TabularCloudPredictor.generate_default_permission(
+            backend=backend, account_id="foo", cloud_output_bucket="foo", output_path=root
+        )
         trust_relationship_path, iam_policy_path = paths["trust_relationship"], paths["iam_policy"]
         for path in [trust_relationship_path, iam_policy_path]:
             with open(path, "r") as file:
