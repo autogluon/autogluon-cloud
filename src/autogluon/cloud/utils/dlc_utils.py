2f7feb832f317d21b8d4954fa5686614ec53137f
Oleksandr Shchur <shchuro@amazon.com>
2026-05-08 15:46:11 +0000

    Fix json

--- src/autogluon/cloud/utils/dlc_utils.py
+++ src/autogluon/cloud/utils/dlc_utils.py
@@ -18,6 +18,21 @@ def _is_gpu_instance(instance_type):
 
 
 def retrieve_available_framework_versions(framework_type="training", details=False):
+    """Get available versions of autogluon
+
+    Args:
+        framework_type (str, optional):
+            Type of framework. Options: 'training', 'inference'.
+            Defaults to 'training'.
+        details (bool, optional):
+            Whether to get detailed information of each versions.
+            Defaults to False.
+
+    Returns:
+        (Union(list, dict)):
+            returns a list of versions if detailed == False.
+            returns a dict containing information related to each version if detailed == True.
+    """
     assert framework_type in ["training", "inference"]
     config = _load_config()
     versions_details = config[framework_type]["versions"]
@@ -32,6 +47,17 @@ def retrieve_py_versions(framework_version, framework_type="training"):
 
 
 def retrieve_latest_framework_version(framework_type="training"):
+    """Get latest version of autogluon framework and its py_versions
+
+    Args:
+        framework_type (str, optional):
+            Type of framework. Options: 'training', 'inference'.
+            Defaults to 'training'.
+
+    Returns:
+        (str, list):
+            version number of latest autogluon framework, and its py_versions as a list
+    """
     versions = retrieve_available_framework_versions(framework_type)
     versions.sort(key=version.parse)
     versions = [(v, retrieve_py_versions(v, framework_type)) for v in versions]
