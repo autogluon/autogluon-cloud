dfd5cbd3d3f9399618a92f4019ead134b3841ee8
Oleksandr Shchur <oleks.shchur@gmail.com>
2026-08-07 15:02:34 +0200

    Add CI concurrency control and tidy version/copyright (#266)

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@ sys.path = [".", ".."] + sys.path
 
 project = "AutoGluon-Cloud"
 release = "0.5.1"
-copyright = "2023, All authors. Licensed under Apache 2.0."
+copyright = "2026, All authors. Licensed under Apache 2.0."
 author = "AutoGluon contributors"
 
 extensions = [
